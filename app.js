74eaf6948e8e2f364d272e600a237996c01ee2b9
anooshaseelm <anoosha.seelam15@gmail.com>
2020-10-27 14:55:24 +0530

    added node project

---
+++ app.js
@@ -0,0 +1,61 @@
+const request = require("request");
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const { static } = require("express");
+
+var app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+})
+
+app.post("/", function(req, res){
+    console.log(req.body.Firstname);
+    var FirstName = req.body.Firstname;
+    var LastName = req.body.Lastname;
+    var email = req.body.email;
+    var data = {
+        members : [
+            {
+            email_address : email,
+            status : "subscribed",
+            merge_fields : {
+                FNAME : FirstName,
+                LNAME : LastName
+            }
+            }
+        ]
+    };
+
+    var jsonDATA = JSON.stringify(data);
+    const listID = "ec792ed7aa";
+    var url = "https://us2.api.mailchimp.com/3.0"+"/lists/"+listID;
+    const options = {
+        method : "POST",
+        auth : "anoo6627:5944368352b8c76b0f995bf58e6c0a3d-us2"
+
+    }
+    const request = https.request(url, options, function(response){
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonDATA);
+    request.end();
+
+});
+
+app.listen(3000,function(){
+    console.log("Server up and running at 3000");
+})
+
+
+//API key
+// 5944368352b8c76b0f995bf58e6c0a3d-us2
+
+//unique ID
+//ec792ed7aa
